767341112b859914567341726557c39d35344250
chichano25 <c.montoya@icloud.com>
2026-03-27 12:34:49 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,28 @@
-const CACHE = 'itc-padel-v1';
+const CACHE_NAME = 'itc-padel-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './service-worker.js',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './club-logo.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
